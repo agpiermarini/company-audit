17e1c8bf531a88d324119a1b21c888c670e3def0
agpiermarini <piermarini.andrew@gmail.com>
2018-02-27 09:51:42 -0700

    working on iteration one

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,10 @@
 class Employee
+  attr_reader :id, :name, :role, :start_date, :end_date
+  def initialize(id, name, role, start_date, end_date)
+    @id = id.to_i
+    @name = name
+    @role = role
+    @start_date = start_date
+    @end_date = end_date
+  end
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -1,13 +1,32 @@
+require 'date'
 require './test/test_helper'
 require './lib/employee'
 
 class EmployeeTest < Minitest::Test
   def test_instantiation
-    employee = Employee.new
+    employee_id = '5'
+    name = 'Sally Jackson'
+    role = 'Engineer'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    employee = Employee.new(employee_id, name, role, start_date, end_date)
 
     assert_instance_of Employee, employee
   end
 
   def test_attributes
+    employee_id = '5'
+    name = 'Sally Jackson'
+    role = 'Engineer'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    employee = Employee.new(employee_id, name, role, start_date, end_date)
+
+    assert_equal employee_id.to_i, employee.id
+    assert_instance_of Integer, employee.id.class
+    assert_equal name, employee.name
+    assert_equal role, employee.role
+    assert_equal Date.new(2015, 1, 1), employee.start_date
+    assert_equal Date.new(2018, 1, 1), employee.end_date
   end
 end
